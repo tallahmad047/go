cb137734e5c70902af7acc363acb8e2ac54c134d
Tall <105008953+tallahmad047@users.noreply.github.com>
2022-07-20 11:28:49 +0000

    Add files via upload

---
+++ pratice-crud-go-main/pratice-crud-go-main/go.mod
@@ -0,0 +1,7 @@
+module exemple/pratice-crud-go-main
+
+go 1.18
+
+require github.com/go-sql-driver/mysql v1.6.0
+
+
---
+++ pratice-crud-go-main/pratice-crud-go-main/main.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Posts struct {
+	Id   int
+	Name string
+	City string
+}
+
+func dbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := ""
+	dbName := "goblog"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+var tmpl = template.Must(template.ParseGlob("form/*"))
+
+//le fonction suivant permet de lister les element de la base de doner
+func Index(w http.ResponseWriter, _ *http.Request) {
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM posts ORDER BY id DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Posts{}
+	res := []Posts{}
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+		res = append(res, emp)
+	}
+	tmpl.ExecuteTemplate(w, "Index", res)
+	defer db.Close()
+}
+
+//le fonction suivant permet de rechercher un element de la base de doner
+func Recherche(w http.ResponseWriter, _ *http.Request) {
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM posts p,categorie c WHERE p.id=c.id_post ORDER BY id DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Posts{}
+	res := []Posts{}
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+		res = append(res, emp)
+	}
+	tmpl.ExecuteTemplate(w, "Index", res)
+	defer db.Close()
+}
+
+//le fonction suivant permet de lister un  element de la base de doner et de voir le deteil
+func Show(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM posts WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Posts{}
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+	}
+	tmpl.ExecuteTemplate(w, "Show", emp)
+	defer db.Close()
+}
+
+func New(w http.ResponseWriter, _ *http.Request) {
+	tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+//le fonction suivant permet de modifier un  element de la base de doner
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM posts WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Posts{}
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+	}
+	tmpl.ExecuteTemplate(w, "Edit", emp)
+	defer db.Close()
+}
+
+//le fonction suivant permet d'inseret les element de la base de doner
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		city := r.FormValue("city")
+		insForm, err := db.Prepare("INSERT INTO posts(name, city) VALUES(?,?)")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, city)
+		log.Println("INSERT: Name: " + name + " | City: " + city)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+//le fonction suivant permet de modifier un element de la base de doner
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		city := r.FormValue("city")
+		id := r.FormValue("uid")
+		insForm, err := db.Prepare("UPDATE posts SET name=?, city=? WHERE id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, city, id)
+		log.Println("UPDATE: Name: " + name + " | City: " + city)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+//le fonction suivant permet de supprimer les element de la base de doner
+func Delete(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	emp := r.URL.Query().Get("id")
+	delForm, err := db.Prepare("DELETE FROM posts WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	delForm.Exec(emp)
+	log.Println("DELETE")
+	defer db.Close()
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+//les roots
+func main() {
+	log.Println("Server started on: http://localhost:8080")
+	http.HandleFunc("/", Index)
+	//http.HandleFunc("/recherche", Recherche)
+	http.HandleFunc("/show", Show)
+	http.HandleFunc("/new", New)
+	http.HandleFunc("/edit", Edit)
+	http.HandleFunc("/insert", Insert)
+	http.HandleFunc("/update", Update)
+	http.HandleFunc("/delete", Delete)
+	http.ListenAndServe(":8080", nil)
+}
